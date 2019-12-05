7f595d8e5dc66af8fd70c22e97db0f1cb470f82b
pabje <33432192+pabje@users.noreply.github.com>
2019-12-05 19:49:07 -0300

    Initial commit

---
+++ DIBB-100/Inicio.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace DIBB_100
+{
+    partial class Inicio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnabrir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnabrir
+            // 
+            this.btnabrir.Location = new System.Drawing.Point(232, 272);
+            this.btnabrir.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnabrir.Name = "btnabrir";
+            this.btnabrir.Size = new System.Drawing.Size(181, 60);
+            this.btnabrir.TabIndex = 0;
+            this.btnabrir.Text = "Abrir";
+            this.btnabrir.UseVisualStyleBackColor = true;
+            this.btnabrir.Click += new System.EventHandler(this.Btnabrir_Click);
+            // 
+            // Inicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 381);
+            this.Controls.Add(this.btnabrir);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.MaximizeBox = false;
+            this.Name = "Inicio";
+            this.Text = "Inicio";
+            this.Load += new System.EventHandler(this.Inicio_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnabrir;
+    }
+}
+
---
+++ DIBB-100/Inicio.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.FileIO;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace DIBB_100
+{
+    public partial class Inicio : Form
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+
+        private void Inicio_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btnabrir_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog buscar = new OpenFileDialog();
+            if(buscar.ShowDialog() == DialogResult.OK)
+            {
+                SqlConnection cn;
+                //string con;
+                //tratar archivo csv
+                string dir = buscar.FileName;
+                int numfac,f;
+                string sernum;
+                int soptype;
+                if (Path.GetExtension(dir) == ".csv")
+                {
+                    //trato el archivo .csv
+                    using (TextFieldParser parser = new TextFieldParser(@dir))
+                    {
+                        parser.TextFieldType = FieldType.Delimited;
+                        parser.SetDelimiters(";");
+                        f = 0;
+                        //SqlConnection cn = new SqlConnection("Persist Security Info=False;User ID=FECOL1;Password=fecol1;Initial Catalog=COL10;Server=10.1.1.22");
+                        cn = new SqlConnection(ConfigurationManager.ConnectionStrings["DIBB100"].ConnectionString);
+                        cn.Open();
+                        while (!parser.EndOfData)
+                        {
+                            //Processing row
+                            string[] fields = parser.ReadFields();
+                            if (f > 0)
+                            {
+                                soptype = 3;
+                                numfac = int.Parse(fields[1]);                                    
+                                sernum = fields[5] + fields[6];                                
+                                try
+                                {
+                                    //using (SqlConnection cn = new SqlConnection("Persist Security Info=False;User ID=sa;Password=1234;Initial Catalog=getty;Server=LAPTOP-0H8OVDMS"))
+                                    SqlCommand cmd = new SqlCommand("SP_ACTUALIZARNUMEROFISCAL", cn);
+                                    cmd.CommandType = CommandType.StoredProcedure;
+                                    cmd.Parameters.AddWithValue("@SOPTYPE", soptype);
+                                    cmd.Parameters.AddWithValue("@NUMFAC", numfac);
+                                    cmd.Parameters.AddWithValue("@SERNUM", sernum);
+                                    SqlParameter MENS = new SqlParameter("@MENS", SqlDbType.VarChar,200);                                    
+                                    MENS.Direction = ParameterDirection.Output;
+                                    cmd.Parameters.Add(MENS);
+                                    cmd.ExecuteScalar();
+                                    //cmd.ExecuteNonQuery();
+                                    MessageBox.Show(MENS.Value.ToString() + ": " + numfac);
+                                    //MessageBox.Show(MENS.Value.ToString() + ": " + numfac);
+                                }
+                                catch (Exception ex)
+                                {
+                                    MessageBox.Show(ex.Message);
+                                }                                
+                            }
+                            f++;
+                        }
+                        cn.Close();
+                    }
+                }
+                else
+                    MessageBox.Show("Error: no abrio un archivo .csv");
+            }
+        }
+    }
+}
